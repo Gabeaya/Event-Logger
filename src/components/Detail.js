268aace4683740de9947635016c8d5d8a50d16a1
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 20:33:21 -0800

    Provide a component for showing details>

---
+++ src/components/Detail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Detail(props){
+  return (
+    <React.Fragment>
+      <h1> Detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Detail;
