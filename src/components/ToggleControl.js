0ecb731b93d235c9c0f0eeae2c17d8674e660285
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 19:51:43 -0800

    Add a constructor reading and export.

---
+++ src/components/ToggleControl.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Form from './Form';
+import SynchronicityList from './SynchronicityList';
+
+class ToggleControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage:false
+    };
+
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default ToggleControl;
