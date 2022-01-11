059e57da3dade97a7cf0664b13df0e275294f12c
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 19:47:57 -0800

    Hard code data.

--- src/components/SynchronicityList.js
+++ src/components/SynchronicityList.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Synchronicity from "./Synchronicity";
 
+const mainSynchronicityList = [
+  {
+    title: 'Repeating Numbers',
+    date: "Feb. 10th 1996",
+    occurence: 'I saw 777 three times today and four times yesterday.',
+    interpretation: 'I am aligning to my spiritual self.'
+  }
+];
+
 function SynchronicityList(){
   return (
+    
     <React.Fragment>
       <hr />
-      {mainSynchronicityList.map((synchronicity) => 
+      {mainSynchronicityList.map((synchronicity, index) => 
         <Synchronicity title={synchronicity.title}
         date={synchronicity.date}
-        occurance={synchronicity.occurence}
-        interpretation={synchronicity.interpretation}/>
+        occurence={synchronicity.occurence}
+        interpretation={synchronicity.interpretation}
+        key={index}/>
       )}
     </React.Fragment>
   );
