0b2f481f97f3795a34b715fafc1e187359e0f506
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 19:00:12 -0800

    Use props from event list and declare proptypes .

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-function Event(){
+function Event(props){
   return(
     <React.Fragment>
-      <h3>{title}</h3>
-      <h3>{date}</h3>
-      <p>{synchronicity}</p>
-      <p>{interpretation}</p>
+      <h3><b>{props.title}</b></h3>
+      <h3><em>{props.date}</em></h3>
+      <p>{props.synchronicity}</p>
+      <p>{props.interpretation}</p>
       <hr/>
     </React.Fragment>
   );
 }
-
+Event.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  synchronicity: PropTypes.string,
+  interpretation: PropTypes.string
+}
 export default Event;
