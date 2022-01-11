63d49a260844dd6394fd62c379b9f2be60010093
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 20:11:02 -0800

    Remove the hard caoded data.

--- src/components/SynchronicityList.js
+++ src/components/SynchronicityList.js
@@ -1,14 +1,7 @@
 import React from "react";
 import Synchronicity from "./Synchronicity";
 
-const mainSynchronicityList = [
-  {
-    title: 'Repeating Numbers',
-    date: "Feb. 10th 1996",
-    occurence: 'I saw 777 three times today and four times yesterday.',
-    interpretation: 'I am aligning to my spiritual self.'
-  }
-];
+
 
 function SynchronicityList(){
   return (
