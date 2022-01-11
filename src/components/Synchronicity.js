29df620962e031919e6cfdd03ba9f8b667052ead
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 19:47:36 -0800

    updat the naming to be correct.

--- src/components/Synchronicity.js
+++ src/components/Synchronicity.js
@@ -1,19 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Synchronicity(props){
   return(
     <React.Fragment>
       <h3><b>{props.title}</b></h3>
       <h3><em>{props.date}</em></h3>
-      <p>{props.occurence}</p>
-      <p>{props.interpretation}</p>
+      <p><b>Event:</b> {props.occurence}</p>
+      <p><b>Meaning:</b> {props.interpretation}</p>
       <hr/>
     </React.Fragment>
   );
 }
-Event.propTypes = {
+Synchronicity.propTypes = {
   title: PropTypes.string.isRequired,
-  date: PropTypes.string.instanceOf(Date),
+  date: PropTypes.string,
   occurence: PropTypes.string.isRequired,
   interpretation: PropTypes.string.isRequired
 }
