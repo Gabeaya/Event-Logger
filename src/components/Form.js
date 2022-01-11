7a5e090f4dfe9903264698c36f7cadfa69beb7a2
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-10 20:10:43 -0800

    Add form inputs and an event handler.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,32 @@
 import React from "react";
-
+import { v4 } from 'uuid';
 function Form(props){
+  function handleFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.title.value);
+    console.log(event.target.date.value);
+    console.log(event.target.occurence.value);
+    console.log(event.target.interpretation.value)
+  }
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleFormSubmission}>
+        <input
+          type='text'
+          name='title'
+          placeholder='Name your event' />
+        <input
+          type='text'
+          name='date'
+          placeholder='date' />
+        <textarea
+          name='occurence'
+          placeholder='Describe your event.' />
+        <textarea
+          name='interpretation'
+          placeholder='Describe what it means to you.' />
+        <button type='submit'>Add Synchronicity!</button>
+      </form>
     </React.Fragment>
   );
 }
