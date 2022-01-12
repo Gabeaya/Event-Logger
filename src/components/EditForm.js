1d6956b2fd22818f134a3e3b671f1482dd52db72
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-11 20:11:28 -0800

    Add an event handler that targets the user inputs.

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -3,9 +3,16 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
 function EditForm (props) {
+  const { synchronicity } = props;
+
+  function handleEditFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({title: event.target.title.value, date: event.target.date.value, occurance: event.target.occurance.value, interpretation: event.target.interpretation.value, id: ticket.id});
+  }
   return (
     <React.Fragment>
       <ReusableForm 
+        formSubmissionHandler={handleEditFormSubmission}
         buttonText="Update" />
     </React.Fragment>
   );
