70905f089bbbf45dacd214b0a64418d45b261f95
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-11 19:27:14 -0800

    Add an edit form.

---
+++ src/components/EditForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+function EditForm (props) {
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        buttonText="Update" />
+    </React.Fragment>
+  );
+}
+
+export default EditForm;
