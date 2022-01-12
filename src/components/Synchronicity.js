8e3826b474a9a38d6bc96e8eda90c06aa7321104
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-11 18:40:10 -0800

    Add an onclick for each synchronicity.

--- src/components/Synchronicity.js
+++ src/components/Synchronicity.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Synchronicity(props){
   return(
     <React.Fragment>
-      <div onClick = {() => props.whenSynchronicityClicked(prop.id)}>
+      <div onClick = {() => props.whenSynchronicityClicked(props.id)}>
         <h3><b>{props.title}</b></h3>
         <h3><em>{props.date}</em></h3>
         <p><b>Event:</b> {props.occurence}</p>
