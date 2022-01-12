209ca45e7a58975ddf5d1e55c8f4ab00459a89b9
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-11 18:29:26 -0800

    use object destructuring and add proptypes.

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,12 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Detail(props){
+  const { synchronicity } = props;
+
   return (
     <React.Fragment>
-      <h1> Detail</h1>
+      <h1>Detail</h1>
+      <h3>{synchronicity.title} on {synchronicity.date}</h3>
+      <p>What happened: {synchronicity.occurance}</p>
+      <p>Interpretation: {synchronicity.interpretation}</p>
       <hr/>
     </React.Fragment>
   );
 }
-
+Detail.propTypes = {
+  synchronicity: PropTypes.object
+};
 export default Detail;
